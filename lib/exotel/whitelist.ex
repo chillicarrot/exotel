6ad5705d4f5bf483f4b0122a59fddf3c589ad8ee
Sushruth Sivaramakrishnan <sivsushruth@gmail.com>
2018-01-02 08:38:33 +0530

    add_to_whitelist

--- lib/exotel/whitelist.ex
+++ lib/exotel/whitelist.ex
@@ -1,5 +1,7 @@
 defmodule Exotel.Whitelist do
-  def add_to_whitelist(%{virtual_number: _virtual_number, numbers: _numbers} = data) do
+  def add_to_whitelist(%{virtual_number: _virtual_number, numbers: numbers} = data) do
+    numbers = Enum.join(numbers, ", ")
+    data = data |> Map.put(:number, numbers) |> Map.delete(:numbers)
     Exotel.API.post("/CustomerWhitelist.json", data)
   end
 end
