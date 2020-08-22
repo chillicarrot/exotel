d5d7bab944818434becd55202ef08be060a4dab1
Sudaakeran Balasubramanian <sudaakeran.b@gmail.com>
2020-08-22 13:09:09 +0530

    seperate auth_key and sid Also no need to handle error as anyways the call will fail

--- lib/exotel/util.ex
+++ lib/exotel/util.ex
@@ -7,9 +7,6 @@ defmodule Exotel.Util do
   def get_base_url() do
     config = Application.get_all_env(:exotel) |> Enum.into(%{})
     api_version = Map.get(config, :api_version, "v1")
-    case {Map.get(config, :sid), Map.get(config, :token)} do
-      {a, b} when is_nil(a) or is_nil(b) -> raise "Require credentials for Exotel API calls."
-      {sid, token} -> "https://#{sid}:#{token}@api.exotel.com/#{api_version}/Accounts/#{sid}/"
-    end
+    "https://#{Map.get(config, :auth_key)}:#{Map.get(config, :auth_token)}@api.exotel.com/#{api_version}/Accounts/#{Map.get(config, :sid)}/"
   end
 end
