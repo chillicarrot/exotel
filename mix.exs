50397ec27675e4c438a4b56fc3cebff59c401a40
Sudaakeran Balasubramanian <sudaakeran.b@gmail.com>
2020-08-22 13:10:18 +0530

    version change

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Exotel.Mixfile do
   def project do
     [
       app: :exotel,
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps()
